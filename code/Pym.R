e59fa269278b151ff985a8418d19b4fa6beed720
[Dan Gunn] <dangunn.studio@gmail.com>
2023-07-31 15:19:48 -0400

    Updating Pym

--- code/Pym.R
+++ code/Pym.R
@@ -3,6 +3,7 @@ install.packages("openNLPdata")
 install.packages("rJava")
 
 library(tidyverse)
+library(NLP)
 library(openNLP)
 library(openNLPdata)
 library(rJava)
@@ -28,7 +29,20 @@ pym <- as.character(pym[2])
 
 #break up into chapters.
 str_count(pym, "CHAPTER") #24 chapters not including the preface
-
 pym_chapters <- str_split_1(pym, "CHAPTER")
 
-pym_chapters[2]
+pym_excerpt <- as.character("Augustus thoroughly entered into my state of mind. It is probable, indeed, that our intimate communion had resulted in a partial interchange of character.  About eighteen months after the period of the Ariel's disaster, the firm of Lloyd and Vredenburgh (a house connected in some manner with the Messieurs Enderby, I believe, of Liverpool) were engaged in repairing and fitting out the brig Grampus for a whaling voyage.")
+
+
+# Load the POS tagger model
+pos_tag_model <- Maxent_POS_Tag_Annotator()
+
+# Tokenize the text
+tokens <- tokenize("raw_data/pym.txt")
+tokens[1:10]
+
+# Perform POS tagging
+tags <- annotate(tokens, pos_tag_model)
+
+# Print the result
+print(tags)
